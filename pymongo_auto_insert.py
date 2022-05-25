95a7a601c5f4f725ef0d12fae503f00c460aaffc
Onur Akduman <85958845+0nurakduman@users.noreply.github.com>
2022-05-25 13:56:26 +0300

    Update pymongo_auto_insert.py

--- pymongo_auto_insert.py
+++ pymongo_auto_insert.py
@@ -7,24 +7,34 @@ user_table = mydb["users"]
 
 while True:
 
- print("Yapılacak İşlemi Seçin.")
- print("=======================")
+ print("Select Action To Do.")
+ print("====================================")
  print("1.Insert")
  print("2.Delete")
+ print("3.Delete")
 
- secim = input("Seçiminiz (1/2):")
+ option = input("Your choice (1/2/3):")
 
- if secim == '1':
-   Insert = input("Kaç adet veri girilmesini istersiniz? ")  
+ if option == '1':
+   Insert = input("How many data do you want to insert? ")  
    for i in range(int(Insert)):
     mydict = { "email": "test_try@araskargo.com", "name": "Test_User", "password": "r=X2KM6+~w;Z)x{K", "rememberMe": "remember-me", "role": "Customer" }
     x = user_table.insert_one(mydict)
     print(x)
 
  
- elif secim == '2':
-   Delete = input("Kaç adet veri girilmesini istersiniz? ")  
+ elif option == '2':
+   Delete = input("How many data do you want deleted? ")  
    for i in range(int(Delete)):
     mydict = { "email": "test_try@araskargo.com", "name": "Test_User", "password": "r=X2KM6+~w;Z)x{K", "rememberMe": "remember-me", "role": "Customer" }
     x = user_table.delete_one(mydict)
     print(x)
+ 
+ elif option == '3':
+   Update = input("How much would you like to update? ")  
+   for i in range(int(Update)):
+    myquery = { "password": "r=X2KM6+~w;Z)x{K", }
+    newvalues = { "$set": { "password": "/fx;[JQ59tXmg7!x" } }
+    x = user_table.update_one(myquery,newvalues)
+    print(x)
+
